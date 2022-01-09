f2465dac67f8a6ffa16955841f75fe3796a15071
Peeyush Yadav <peeyushyadav1402@gmail.com>
2022-01-10 02:07:15 +0530

    Ui Changes and Added Data

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faPlay,faAngleLeft,faAngleRight} from '@fortawesome/free-solid-svg-icons'
 
-function Player() {
+function Player({currentSong}) {
+    //References
+    const audioRef = useRef(null);
+
+    //Event Handlers
+    const playSongHandler = () =>{
+        console.log(audioRef);
+    } 
+
+
+
+
     return (
-        <div>
+        <div className="player">
             <div className="time-control">
                 <p>Start Time</p>
                 <input type="range" />
@@ -13,9 +24,11 @@ function Player() {
 
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay}/>
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
             </div>
+
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
 }
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function Song() {
+function Song({currentSong}) {
     return (
         <div className="song-container">
-            
+            <img src={currentSong.cover}></img>
+            <h2>{currentSong.name}</h2>
+            <h3>{currentSong.artist}</h3>
         </div>
     )
 }
